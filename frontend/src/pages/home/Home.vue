92c4ce18e5da482e136569395168ce6cfe355504
haind <hainguyenduy1010@gmail.com>
2020-03-18 22:41:17 +0700

    [FE] set background image for home page, new logo, favico

--- frontend/src/pages/home/Home.vue
+++ frontend/src/pages/home/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" class="container-home">
     <div class="container">
 
         <div class="home-logo">
--- frontend/src/pages/search-result/SearchResult.vue
+++ frontend/src/pages/search-result/SearchResult.vue
@@ -58,7 +58,7 @@
 										</a>
 									</h3>
 									<a class="search-link" target="_blank" v-bind:href="search_data.url" style="color: #006621;">
-										{{search_data.url.slice(0, 60) + '...'}}
+										{{search_data.url.length > 60 ? search_data.url.slice(0, 60) + '...' : search_data.url}}
 									</a>
 									<p v-html="search_data.description"></p>
 								</div>
